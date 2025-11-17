9b69c537e3fbcf476dd7e7bb1aabb358e8d44196
Gerard <gerardmorsilvestre@gmail.com>
2025-11-17 14:06:37 +0100

    g

--- src/com/utils/forms/FormsBuilder.java
+++ src/com/utils/forms/FormsBuilder.java
@@ -3,17 +3,21 @@ package com.utils.forms;
 public class FormsBuilder {
     public static void printTitle(String msg){
         System.out.println(msg);
+
         for (int i = 0; i < msg.length(); i++) {
             System.out.print("=");
         }
         System.out.println();
     }
 
-    public static void printValue(String key, String value){
-        System.out.println(key + ": " + value);
+    public static void printValue(String Key, String Value){
+        
+        System.out.println(Key + ": " + Value);
+    
     }
 
     public static void divider(char c){
+
         for (int i = 0; i < 20; i++) {
             System.out.print(c);
         }
--- src/formulari.java
+++ src/formulari.java
@@ -1,16 +1,39 @@
 import com.utils.forms.FormsBuilder;
+import java.util.Scanner;
 public class formulari {
     public static void main(String[] args) {
-        FormsBuilder.printTitle("Formulari de Registre d'Usuaris");
-        FormsBuilder.printValue("Nom", "Joan Pérez");
-        FormsBuilder.printValue("Edat", "30");
-        FormsBuilder.printValue("Correu Electrònic", "joan.perez@example.com");
+        Scanner as = new Scanner(System.in);
+        System.out.println("Digues el teu nom:");
+        String nom = as.nextLine();
+        System.out.println("Digues la teva edat:");
+        String edat = as.nextLine();
+        System.out.println("Digues el teu correu electrònic:");
+        String correu = as.nextLine();
+        System.out.println("Digues la quota que pagues:");
+        String quota = as.nextLine();
+        
+        //if (quota >= 0,25){
+        //    System.out.println("No tens descompte");
+        //} else if (quota <= 0,25 && quota >= 0,75){
+        //    System.out.println("Tens un descompte del 5%");
+        //} else if (quota <= 0,75 && quota >=1){
+        //    System.out.println("Tens un descompte del 10%");
+        //}  
+        
+        FormsBuilder.printTitle("Soci Forn de pa");
+        FormsBuilder.printValue("Nom", nom);
+        FormsBuilder.printValue("Edat", edat);
+        FormsBuilder.printValue("Correu Electrònic", correu);
+        FormsBuilder.printValue("Quota que paga", quota);
         FormsBuilder.divider('-');
-        FormsBuilder.printTitle("Informació Addicional");
-        FormsBuilder.printValue("País", "Espanya");
-        FormsBuilder.printValue("Ciutat", "Barcelona");
+        FormsBuilder.printValue("Nom", nom);
+        FormsBuilder.printValue("Edat", edat);
+        FormsBuilder.printValue("Correu Electrònic", correu);
+        FormsBuilder.printValue("Quota que paga", quota);
+        FormsBuilder.printValue("Validació", "Si/No");
         FormsBuilder.divider('-');
-        FormsBuilder.printValue("Registre Complet", "Gràcies per registrar les teves dades!");
+        FormsBuilder.printValue("El soci", "Gràcies per registrar les teves dades!");
+        FormsBuilder.printValue("Descompte", "es calcula segons la quota que paga");
 
 
     }
